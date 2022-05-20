27f1baf5d956947b7a6b617b2f5c7390f2f17d07
zein3 <zeinhaddad02@gmail.com>
2022-05-21 06:01:38 +0700

    progress on post.show ui and remade navbar ui

--- resources/views/components/layouts/navbar.blade.php
+++ resources/views/components/layouts/navbar.blade.php
@@ -2,17 +2,45 @@
         {{--Navbar--}}
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container">
-                <button class="btn btn-outline-secondary d-lg-none my-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar">
-                    <i class="bi bi-list"></i> 
-                </button>
-                <a class="navbar-brand text-white bg-dark px-2 border rounded d-none d-lg-block" href="{{ route('home') }}">
-                    Blog
-                </a>
-                <div class="form-control py-0 px-0 d-flex flex-row align-items-center">
-                    <input class="border-0 flex-grow-1 p-1" placeholder="search...">
-                    <button class="btn btn-outline-primary">
-                        <i class="bi bi-search"></i>
+                <div class="d-flex align-items-center justify-content-between w-100">
+                    <button class="btn btn-outline-secondary d-lg-none my-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar">
+                        <i class="bi bi-list"></i> 
                     </button>
+                    <a class="navbar-brand text-white bg-dark px-2 border rounded d-none d-lg-block" href="{{ route('home') }}">
+                        Blog
+                    </a>
+                    <div class="d-none d-lg-block">
+                        <div class="form-control py-0 px-0 d-flex flex-row align-items-center">
+                            <input class="border-0 flex-grow-1 p-1" placeholder="search...">
+                            <button class="btn btn-outline-primary">
+                                <i class="bi bi-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                    
+                    @auth
+                    <div class="dropstart">
+                        <a class="m-1" href="#" role="button" id="profileDropdown" data-bs-toggle="dropdown">
+                            <img class="rounded-circle" src="https://i.pravatar.cc/50?img=3" width="50" height="50" />
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="profileDropdown">
+                            <li>
+                                <a href="#" class="dropdown-item">
+                                    My Profile
+                                </a>
+                            </li>
+                            <li>
+                                <form action="{{ route('logout') }}">
+                                    <button type="submit" class="dropdown-item">
+                                        Log out
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                    @endauth
                 </div>
+                
             </div>
         </nav>
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,6 +7,9 @@
 
     <div class="container min-vh-100 mt-4">
         <div class="row">
+            <div class="d-lg-none">
+                <x-layouts.sidebar />
+            </div>
             <div class="col-lg-8">
                 <div class="card shadow">
                     <img src="{{ $post->thumbnail }}" class="card-img-top" style="max-height: 10rem;" />
@@ -19,9 +22,32 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4" style="position: sticky; top: 1rem;">
+                <hr />
+                @foreach($post->comments as $comment)
+                <div class="card shadow my-2">
+                    <div class="card-body">
+                        <div class="d-flex flex-row align-items-center">
 
+                        </div>
+                        {{ $comment->body }}
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <div class="col-lg-4">
+                <div style="position: sticky; top: 1rem;">
+                    <div class="card shadow">
+                        <div class="card-body px-4 py-2">
+                            <h4 class="h4 fw-bold">
+                                Author:
+                            </h4>
+                            <div class="d-flex align-items-center justify-content-between">
+                                <img src="{{ $post->author->profile_picture }}" class="rounded-circle" width="50" height="50" />
+                                <a href="#" class="text-decoration-none text-black fw-bold fs-3">{{ $post->author->full_name }}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>

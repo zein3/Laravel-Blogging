043b6fdf33db1b6f11bed75ce8980b7bbc202e3f
zein3 <zeinhaddad02@gmail.com>
2022-05-29 14:09:38 +0700

    added alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,20 @@
 
         <x-layouts.navbar />
 
+
         <div class="container min-vh-100">
+
+            @if(session('message'))
+            <div class="my-2 alert alert-primary alert-dismissable fade show" role="alert">
+                <div class="d-flex w-100 justify-content-between align-items-center">
+                    <h4>
+                        {{ session('message') }}
+                    </h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+            @endif
+
             <div class="row min-vh-100">
                 
                 <x-layouts.sidebar />
